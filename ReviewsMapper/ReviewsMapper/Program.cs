537be862e47463dcc8d14f3bbfb0d317a0efb9bd
navule <pavankumar.navule@innroad.com>
2017-04-27 14:06:38 +0530

    HDInsight streaming map reducer to get count of reviews by date

---
+++ ReviewsMapper/ReviewsMapper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ReviewsMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            //Hadoop passes data to the mapper on STDIN
+            while ((line = Console.ReadLine()) != null)
+            {
+                // We need date, so strip out everything before last occurence of |||
+                string datefrominput = line.Split(new char[] { '|' }).LastOrDefault();
+                //Format date
+                var dateParts = datefrominput.Replace(",", "").Split(new char[] { ' ' });
+                var formattedDated = $"{dateParts[0]}-{dateParts[1]}-{dateParts[2]}";
+                //write date from every line to the console
+                Console.WriteLine("{0}\t1", formattedDated);
+            }
+        }
+    }
+}
---
+++ ReviewsReducer/ReviewsReducer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewsReducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary for holding a count of dates
+            Dictionary<string, int> dates = new Dictionary<string, int>();
+
+            string line;
+            //Read from STDIN
+            while ((line = Console.ReadLine()) != null)
+            {
+                // Data from Hadoop is tab-delimited key/value pairs
+                var sArr = line.Split('\t');
+                // Get the date
+                string date = sArr[0];
+                // Get the count
+                int count = Convert.ToInt32(sArr[1]);
+
+                //Do we already have a count for the date?
+                if (dates.ContainsKey(date))
+                {
+                    //If so, increment the count
+                    dates[date] += count;
+                }
+                else
+                {
+                    //Add the key to the collection
+                    dates.Add(date, count);
+                }
+            }
+            //sort by decreasing order of count and emit each date and count of reviews
+            foreach (var date in dates.OrderByDescending(key => key.Value))
+            {
+                //Emit tab-delimited key/value pairs.
+                //In this case, a date and a count of 1.
+                Console.WriteLine("{0}\t{1}", date.Key, date.Value);
+            }
+        }
+    }
+}
